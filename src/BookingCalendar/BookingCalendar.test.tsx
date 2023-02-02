74f8e7cb90c5c99d808b9a06e3f3f0794c03b43f
demark-pro <me@demark.pro>
2023-02-03 01:45:17 +0300

    add tests

--- src/BookingCalendar/BookingCalendar.test.tsx
+++ src/BookingCalendar/BookingCalendar.test.tsx
@@ -3,6 +3,7 @@ import { render } from "@testing-library/react";
 
 import BookingCalendar from "./BookingCalendar";
 
+jest.mock('./BookingCalendar.module.css', () => '')
 describe("BookingCalendar", () => {
   test("renders the BookingCalendar component", () => {
     render(<BookingCalendar selectedStart={new Date()} selectedEnd={null} />);
