f76e162286cdfa4d3f8442cc4d142017b7fe788a
demark-pro <me@demark.pro>
2023-02-08 09:18:42 +0300

    Initial pages commit

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import BookingCalendar from "./BookingCalendar/BookingCalendar";
-import { getReservedInfoOfDate } from "./utils/getReservedInfoOfDate";
-import { getSelectedTime } from "./utils/getSelectedTime";
-
-export { BookingCalendar, getReservedInfoOfDate, getSelectedTime };
-
-export default BookingCalendar;
