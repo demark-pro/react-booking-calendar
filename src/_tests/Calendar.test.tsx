0d8e2a268625a4a61a0d931c60a78669a34bf4f6
demark-pro <me@demark.pro>
2023-02-20 23:23:10 +0300

    add css test Calendar

--- src/_tests/Calendar.test.tsx
+++ src/_tests/Calendar.test.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { Calendar } from "..";
+import { createSerializer } from "@emotion/jest";
+
+expect.addSnapshotSerializer(createSerializer());
 
 describe("Calendar", () => {
   test("renders the Calendar component", () => {
     render(<Calendar selected={[new Date(), null]} />);
   });
+});
+
+test("snapshot - Calendar", () => {
+  const { container } = render(
+    <Calendar selected={[]} classNamePrefix="calendar" />
+  );
+  expect(container).toMatchSnapshot();
 });
