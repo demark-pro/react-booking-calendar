395e922970ea9168880e9ed5b47edcbec81dba50
demark-pro <me@demark.pro>
2023-02-22 01:39:46 +0300

    correct import Calendar

--- src/_tests/Calendar.test.tsx
+++ src/_tests/Calendar.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { Calendar } from "..";
+import { Calendar } from "../index";
 import { createSerializer } from "@emotion/jest";
 
 expect.addSnapshotSerializer(createSerializer());
